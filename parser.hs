34a563344859627f761bec5ea67020fb1e9bbf73
Quintium <yzmailer@gmail.com>
2024-03-21 18:22:25 +0100

    Formatting

--- parser.hs
+++ parser.hs
@@ -1,46 +1,52 @@
 module Parser where
 
 import Control.Applicative
-import Data.Char
 import Control.Monad
+import Data.Char
 
 newtype Parser a = Parser {runParser :: String -> Maybe (a, String)}
 
 instance Monad Parser where
-    (>>=) :: Parser a -> (a -> Parser b) -> Parser b
-    (Parser p) >>= f = Parser p'
-        where p' s = do (x1, rest1) <- p s
-                        runParser (f x1) rest1
+  (>>=) :: Parser a -> (a -> Parser b) -> Parser b
+  (Parser p) >>= f = Parser p'
+    where
+      p' s = do
+        (x1, rest1) <- p s
+        runParser (f x1) rest1
 
 instance Functor Parser where
-    fmap :: (a -> b) -> Parser a -> Parser b
-    fmap = liftM
+  fmap :: (a -> b) -> Parser a -> Parser b
+  fmap = liftM
 
 instance Applicative Parser where
-    pure :: a -> Parser a
-    pure x = Parser (\s -> Just (x, s))
+  pure :: a -> Parser a
+  pure x = Parser (\s -> Just (x, s))
 
-    (<*>) :: Parser (a -> b) -> Parser a -> Parser b
-    p1 <*> p2 = do f <- p1
-                   f <$> p2
+  (<*>) :: Parser (a -> b) -> Parser a -> Parser b
+  p1 <*> p2 = do
+    f <- p1
+    f <$> p2
 
 instance Alternative Parser where
-    empty :: Parser a
-    empty = Parser $ const Nothing
+  empty :: Parser a
+  empty = Parser $ const Nothing
 
-    (<|>) :: Parser a -> Parser a -> Parser a
-    (Parser p1) <|> (Parser p2) = Parser p'
-        where p' s = p1 s <|> p2 s
+  (<|>) :: Parser a -> Parser a -> Parser a
+  (Parser p1) <|> (Parser p2) = Parser p'
+    where
+      p' s = p1 s <|> p2 s
 
 finishParser :: Parser a -> String -> Maybe a
-finishParser p s = do (x, rest) <- runParser p s
-                      guard (null rest)
-                      return x
+finishParser p s = do
+  (x, rest) <- runParser p s
+  guard (null rest)
+  return x
 
 oneP :: (Char -> Bool) -> Parser Char
 oneP f = Parser p
-    where p (c:rest) | f c = Just (c, rest)
-          p _              = Nothing
+  where
+    p (c : rest) | f c = Just (c, rest)
+    p _ = Nothing
 
 manyP :: (Char -> Bool) -> Parser String
 manyP f = many (oneP f)
--- prologparser.hs
+++ prologparser.hs
@@ -1,46 +1,55 @@
 module PrologParser where
 
+import Control.Applicative
+import Control.Monad.Reader
+import Control.Monad.State
 import Data.Char
 import Data.List
 import Data.Maybe
-import Control.Applicative
-import Control.Monad.State
-import Control.Monad.Reader
-
-import Types
 import Parser
+import Types
 
 atomP :: Parser TermP
 atomP = (`FunctionP` []) <$> expressionP ",.():-\n"
 
 functionP :: Parser TermP
-functionP = FunctionP <$> (expressionP ",.():-\n" <* spaceP <* charP '(' <* spaceP) <*> sepBy (charP ',') termP <* charP ')'
+functionP =
+  FunctionP
+    <$> (expressionP ",.():-\n" <* spaceP <* charP '(' <* spaceP)
+    <*> sepBy (charP ',') termP
+    <* charP ')'
 
 termP :: Parser TermP
 termP = spaceP *> (functionP <|> atomP) <* spaceP
 
 processTerm :: TermP -> State VarNames Term
-processTerm (FunctionP name []) | isUpper (head name) || head name == '_' = do 
-    (VarNames vn) <- get
-    if name `elem` vn
+processTerm (FunctionP name [])
+  | isUpper (head name) || head name == '_' = do
+      (VarNames vn) <- get
+      if name `elem` vn
         then return $ Var (fromJust (elemIndex name vn))
-        else do put (VarNames (vn ++ [name]))
-                return $ Var (length vn)
-                                | otherwise = return $ Function name []
-processTerm (FunctionP name args) = do 
-    args' <- mapM processTerm args
-    return $ Function name args'
+        else do
+          put (VarNames (vn ++ [name]))
+          return $ Var (length vn)
+  | otherwise = return $ Function name []
+processTerm (FunctionP name args) = do
+  args' <- mapM processTerm args
+  return $ Function name args'
 
 parseTerm :: String -> Maybe (State VarNames Term)
 parseTerm s = do
-    tp <- finishParser termP s
-    return $ processTerm tp
+  tp <- finishParser termP s
+  return $ processTerm tp
 
 factP :: Parser RuleP
 factP = (`RuleP` []) <$> termP <* charP '.'
 
 ruleP :: Parser RuleP
-ruleP = RuleP <$> (termP <* stringP ":-" <* spaceP) <*> sepBy (charP ',') termP <* charP '.'
+ruleP =
+  RuleP
+    <$> (termP <* stringP ":-" <* spaceP)
+    <*> sepBy (charP ',') termP
+    <* charP '.'
 
 programP :: Parser ProgramP
 programP = spaceP *> (ProgramP <$> sepBy spaceP (ruleP <|> factP)) <* spaceP
@@ -52,8 +61,10 @@ processProgram :: ProgramP -> Program
 processProgram (ProgramP rules) = Program $ map processRule rules
 
 processRule :: RuleP -> Rule
-processRule (RuleP head tails) = evalState (Rule <$> processTerm head <*> mapM processTerm tails) emptyVarNames
+processRule (RuleP head tails) =
+  evalState (Rule <$> processTerm head <*> mapM processTerm tails) emptyVarNames
 
 processQuery :: QueryP -> Query
-processQuery (QueryP ts) = let (terms, vn) = runState (mapM processTerm ts) emptyVarNames
-                           in Query terms vn
+processQuery (QueryP ts) =
+  let (terms, vn) = runState (mapM processTerm ts) emptyVarNames
+   in Query terms vn
